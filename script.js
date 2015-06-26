84813c2827183fea14eec4c46c0482ee3a9f07a0
Tony Gustafsson <tony.gustafsson@wipcore.se>
2015-06-26 10:12:11 +0200

    Ability to remove objects from inventory, bug fixes

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var objects = [],
+	svgContainer = document.getElementById("svg-container"),
 	roomImg = 'svg/room.svg';
 
 var getObjects = function () {
@@ -21,10 +22,26 @@ var saveToInventory = function (object) {
 		
 	image.setAttribute('x', 0);
 	image.setAttribute('y', 0);
+	image.setAttribute('data-object-reference', object.id);
+	image.id = 'object-reference-' + object.id;
 	image.classList.remove('invisible');
+	
+	svg.addEventListener('click', function() {
+		//Remove from inventory on click
+		var svgObject = document.getElementById(image.getAttribute('data-object-reference'));
+		removeFromInventory(object);
+		showObject(svgObject);
+	});
 	
 	svg.appendChild(image);
 	inventory.appendChild(svg);
+};
+
+var removeFromInventory = function (object) {
+	var inventory = document.getElementById('inventory'),
+		inventoryObject = document.getElementById('object-reference-' + object.id);
+	
+	inventory.removeChild(inventoryObject.parentNode);
 };
 
 var clearInventory = function (object) {
@@ -33,29 +50,38 @@ var clearInventory = function (object) {
 	inventory.innerHTML = "";
 };
 
+var hideObject = function (object) {
+	object.classList.add('invisible');
+	localStorage.setItem(object.id, "invisible");
+};
+
+var showObject = function (object) {
+	object.classList.remove('invisible');
+	localStorage.removeItem(object.id);
+};
+
 (function loadSvg() {
 	var ajax = new XMLHttpRequest();
 	
 	ajax.open("GET", roomImg, true);
 	ajax.send();
 	ajax.onload = function(e) {
-		var div = document.getElementById("svg-container");
+		var div = svgContainer;
 		div.innerHTML = ajax.responseText;
 		
 		objects = getObjects();
 		
-		[].forEach.call(objects, function(object) {
+		objects.forEach(function(object) {
 			object.classList.add('object');
 			
 			if (localStorage.getItem(object.id) == "invisible") {
-				object.classList.add('invisible');
+				hideObject(object);
 				saveToInventory(object);
 			}
 			
 			object.addEventListener('click', function() {
-				object.classList.add("invisible");
+				hideObject(object);
 				saveToInventory(object);
-				localStorage.setItem(object.id, "invisible");
 			});
 		});
 	};
@@ -65,8 +91,8 @@ var clearInventory = function (object) {
 	document.getElementById('reset-objects').addEventListener('click', function() {
 		localStorage.clear();
 
-		[].forEach.call(objects, function(object) {
-			object.classList.remove("invisible");
+		objects.forEach(function(object) {
+				showObject(object);
 		});
 		
 		clearInventory();
